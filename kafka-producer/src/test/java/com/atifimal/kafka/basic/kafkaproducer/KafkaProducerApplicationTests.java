1353c5c0025e1444bd0c742a437807e8019ef95a
TURKRE\atif.imal <aimal@turkre.com>
2022-05-17 23:31:37 +0300

    Empty spring boot projects

---
+++ kafka-producer/src/test/java/com/atifimal/kafka/basic/kafkaproducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atifimal.kafka.basic.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
